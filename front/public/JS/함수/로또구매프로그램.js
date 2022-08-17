fdbcd69dd678a14857ea524fca97c77d44d87b9c
504t <504t@DESKTOP-DE9LJ86>
2022-08-17 15:54:54 +0900

    day10_로또

--- front/public/JS/함수/로또구매프로그램.js
+++ front/public/JS/함수/로또구매프로그램.js
@@ -1,14 +1,17 @@
 /*
 	로또 구매 
 		1.사용자 6개 버튼 선택한다.
-			조건: 중복배제
+			조건: 한번클릭(선택) 두번클릭(취소) , 최대6개
 		2.컴퓨터 추첨번호[1~45난수] 생성
 		3.사용자 추첨번호 비교 결과
 	
  */
 
+// * 변수 선언 
 // 변수는 지역변수 특징 = { } 안으로 들어갈수 있다. { } 밖으로 못나감  
-let select_number = [ ]	// 여러개 함수에서 호출(사용) 될 예정 
+let select_numbers = [ ] // 내가 선택한 번호 리스트 	// 여러개 함수에서 호출(사용) 될 예정 
+let lotto_numbers = [ ] // 컴퓨터 난수 번호 리스트 
+
 // 1. 버튼 만들기함수 
 function btn_print(  ){
 	let btnlist = '' // 1~45의 버튼을 저장하는 변수 
@@ -23,19 +26,48 @@ function btn_print(  ){
 }
 // 2. 버튼 눌렀을때 함수 [ 매개변수 : i -> 내가 클릭한 버튼의 순서 ]
 function btn_click( i ){
-	// 중복배제
-	if ( select_number.indexOf( i ) >= 0 ){
+	// 배열내 존재하는 숫자 인지 확인 
+	let index = select_numbers.indexOf( i )
+	if ( index >= 0 ){ // 선택한 수가 배열내 존재하면 
 	// 배열명.indexOf( 데이터 ) : 배열내 해당데이터의 인덱스 검색 
 		// 해당 데이터가 존재하면 찾은 인덱스 출력  // 해당 데이터가 존재하지 않으면 -1 출력 
-		alert('이미 선택한 번호입니다. [ 다시 선택 ]')
-		return // 함수 종료 
+		alert('이미 선택한 번호입니다. [ 취소합니다. ]')
+		select_numbers.splice( index  , 1 ) // 배열명.splice( 삭제할인덱스 , 개수 )
+	}else{ // 선택한 수가 배열내 존재하지 않으면 
+		// 배열내 개수 체크 
+		if( select_numbers.length == 6 ){ // 만약에 배열내 숫자가 6개 이면 
+			alert('더 이상 선택할수 없습니다.')
+			return // 함수 종료[ 아래 코드 실행X ]
+		}
+		// 선택한 번호 배열 저장 
+		select_numbers.push( i ) // 배열명.push( 데이터 ) : 배열에 데이터 추가 
 	}
-	// 선택한 번호 배열 저장 
-	select_number.push( i ) // 배열명.push( 데이터 ) : 배열에 데이터 추가 
+	// 내가 선택한 번호들 해당 id태그에 출력 
+	document.getElementById('selectbox').innerHTML = select_numbers
 }
 // 3. 추첨번호 생성 함수 
 function lotto_number(){
-	
+	if( select_numbers.length != 6 ){
+		alert('6개 수를 모두 입력후 추첨 해주세요 ')
+		return // 함수 종료[ 아래 코드 실행X ]
+	}
+	// 난수 생성 [ 1~45 ]
+	while( true ){ // 무한루프 [ 추첨번호가 6개 되면 종료 ]
+														// Math.random()  0~1 사이의 실수 생성 
+		let rand = Math.floor( (Math.random()*45)+1 )   // ( Math.random()*끝값 )+시작값 : 시작값 ~ 끝값 사이의 난수 실수 생성 
+		let index = lotto_numbers.indexOf( rand )		// Math.floor( 난수 ) : 소수점 버림 함수 
+		if( index == -1  ){ // 존재하지 않으면  
+			if( lotto_numbers.length == 6 ){ // 배열내 수가 6개 이면 반복문 종료 
+				break // 가장 가까운 반복문 탈출 
+			}else{ // 배열내 수가 6개 가 아니면 해당 난수를 배열에 저장 
+				lotto_numbers.push( rand )
+			} // else end 
+		} // if end 
+	} // while end 
+	// html 태그 에 배열 출력
+	document.getElementById('lottobox').innerHTML = lotto_numbers
+	// 결과 함수 출력 
+	lotto_result()
 }
 // 4. 추첨번호 비교 결과 함수 
 function lotto_result(){
