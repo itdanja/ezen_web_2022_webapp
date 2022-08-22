06e80fc54af5af5c4a55cf0b38898525ed96f75f
504t <504t@DESKTOP-DE9LJ86>
2022-08-22 15:32:20 +0900

    day13

--- front/public/JS/객체/5.점수관리.js
+++ front/public/JS/객체/5.점수관리.js
@@ -20,12 +20,14 @@
 				- 배열은 다른 함수에서도 사용할 예정 [ 함수 밖에서 선언 ]
 			
 			4. HTML 초기화
+		3. 학생 점수출력 
+			1. js 처음 열렸을때 1번  / 2.학생등록 할때마다
 				
 */
 // 
 
 let studentlist = [ ] // *여러개 학생 객체를 저장하는 배열 [ 함수 밖에 선언한 이유 : 여러 함수들에서 사용하기 위해서]
-
+학생출력()
 
 function 점수등록(){ // 2. 점수 등록 버튼을 눌렀을때
 	// 1. 학생 객체 : HTML 입력받은 데이터로 선언
@@ -51,10 +53,14 @@ function 점수등록(){ // 2. 점수 등록 버튼을 눌렀을때
 	document.getElementById('kor').value = ''
 	document.getElementById('eng').value = ''
 	document.getElementById('mat').value = ''
+	// 5. 
+	학생출력()
+	
 } // f end 
 
 function 이름중복체크( name ) { // 학생 이름 중복체크 함수 정의
 	for( let i = 0 ; i<studentlist.length ; i++ ){
+		// 만약에 배열에 객체가 3개 있을경우 =>  길이 : 1~3		인덱스:0~2
 		// i는 0부터 i는 배열길이[ 배열내 객체수]미만까지 1씩증가 반복 
 		if( studentlist[i].name === name ){
 			// 배열내 i번째 인덱스의 객체내 이름이  새로 입력받은 이름과 같으면
@@ -65,6 +71,37 @@ function 이름중복체크( name ) { // 학생 이름 중복체크 함수 정�
 	return false // 배열내 name 동일한 이름이 없다. 
 } // f end 
 
+function 학생출력() {
+	// 1. 변수에 html태그[문자열]를 저장하기 
+	let html = '<tr>' + 
+					'<th>등수</th>'+
+					'<th>학생명</th>'+
+					'<th>국어</th>'+
+					'<th>영어</th>'+
+					'<th>수학</th>'+
+					'<th>총점</th>'+
+					'<th>평균</th>'+
+				'</tr>'
+	// *** 배열내 객체들을 하나씩 꺼내기 -> 객체 정보를 html 화 
+	for( let i = 0 ; i<studentlist.length ; i++ ){
+		// i는 0부터 배열내 마지막인덱스까지[ 배열의길이-1 ] 1씩 증가반복
+		// 객체에 들어있는 데이터를 html로 출력 
+		html += '<tr>' + 
+					'<th>등수</th>'+
+					'<th>'+studentlist[i].name+'</th>'+
+					'<th>'+studentlist[i].kor+'</th>'+
+					'<th>'+studentlist[i].eng+'</th>'+
+					'<th>'+studentlist[i].mat+'</th>'+
+					'<th>총점</th>'+
+					'<th>평균</th>'+
+				'</tr>' 
+		// 변수는 문자처리 X
+			// 변수는 앞전에 미리 정의된 단어[ 키워드 = 컴퓨터가 알고 있는 단어 ]
+	} // for end 
+	// 2. 해당 변수를 html에 출력하기
+	document.getElementById('listtable').innerHTML = html
+	
+} // f end 
 
 
 
