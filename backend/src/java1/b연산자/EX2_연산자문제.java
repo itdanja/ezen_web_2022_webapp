56c6f80c4c003f942eb4afeed11f9b3829599cae
504t <504t@DESKTOP-DE9LJ86>
2022-09-01 17:05:24 +0900

    day21

--- backend/src/java1/b연산자/EX2_연산자문제.java
+++ backend/src/java1/b연산자/EX2_연산자문제.java
@@ -1,11 +1,25 @@
 package java1.b연산자;
 
+import java.util.Scanner;
+
 public class EX2_연산자문제 { // class s
 	public static void main(String[] args) { // main s
+		// 1. 입력객체 [ 1번만 선언 ] 
+		Scanner scanner = new Scanner(System.in);
+				// scanner.nextInt()  	: 입력된 데이터를 int형으로 가져오기 
+				// scanner.nextDouble() : 입력된 데이터를 double형으로 가져오기 
+				// scanner.next() 		: 입력된 데이터를 String형으로 가져오기 
 		/*
 			문제1 : 급여 계산 
 				[조건] 입력받기 : 기본급 , 수당 
 				[출력] 실수령액 = 기본급 + 수당 - 세금[기본급10%]
+		
+		// 모법답안
+		System.out.print("문제1) 기본급 : "); 	int 기본급 = scanner.nextInt();
+		System.out.print("문제1) 수당 : "); 	int 수당 = scanner.nextInt();
+		int 실수령액 = 기본급 + 수당 - (int)(기본급*0.1);
+								// * 강제 형 변환 : double -> int 
+		System.out.println("문제1) 결과 : " + 실수령액 );
 		 */
 		/*
 		 	문제2 : 지폐 개수 세기 
@@ -16,6 +30,16 @@ public class EX2_연산자문제 { // class s
 					만원 : 5장 
 					천원 : 6장
 		 */
+		// 모범답안 
+		System.out.print("문제2) 금액 : ");	
+		int 금액;   // 변수 선언 하고 자료형 선택 : ??? 1.논리2.정수3.실수4.문자열 
+		금액 = scanner.nextInt();	 // 입력받은 데이터를 정수형으로 가져와서 변수에 대입 
+			// 1. 십만원 계산 
+			System.out.println("십만원 : " + (금액/100000)+"장");
+				금액 -= (금액/100000) * 100000;	// 십만원 단위 제거 
+			System.out.println("만원 : " + (금액/10000) + "장");
+				금액 -= (금액/10000) * 10000; // 만원 단위 제거 
+			System.out.println("천원 : " + (금액/1000) + "장");
 		/*
 		 	문제3 : 하나의 정수를 입력받아 7의 배수 이면 '맞다' 아니면 '아니다' [ if x ]
 		 	문제4 : 하나의 정수를 입력받아 홀수이면 이면 '홀수' 아니면 '짝수' [ if x ]
