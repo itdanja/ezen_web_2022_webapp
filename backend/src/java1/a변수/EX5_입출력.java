aaf097887fe7ef378c980117ec85de09db0bd65a
504t <504t@DESKTOP-DE9LJ86>
2022-09-01 16:04:20 +0900

    day21

--- backend/src/java1/a변수/EX5_입출력.java
+++ backend/src/java1/a변수/EX5_입출력.java
@@ -1,5 +1,7 @@
 package java1.a변수;
 
+import java.util.Scanner;
+
 public class EX5_입출력 { // class s
 	public static void main(String[] args) { // main s
 		
@@ -21,10 +23,42 @@ public class EX5_입출력 { // class s
 		String job ="도적";
 		System.out.printf("%6d | %-10s | %10s\n" , 1 , name , job );
 		
+		// 입력 객체(클래스) 선언 
+		Scanner 입력객체 = new Scanner(System.in);
+		// [객체생성] 클래스명 객체명 = new 생성자();
+						// new 객체 선언시 메모리 할당 키워드
+		int 입력정수 = 입력객체.nextInt();
+		System.out.println(" 입력받은 정수 : " + 입력정수 );
+		
+		String 입력문자열 = 입력객체.next();
+		System.out.println(" 입력받은 문자열 : " + 입력문자열 );
+		
+		/*-----------------------------------*/
+		// 입력예제 ) 
+		// 1. 입력받기[ 준비물 : Scanner 클래스로 객체 1개 필요 ]
+			// 키보드(System.in) --입력--> 객체 저장 --next()--> 변수 저장 
+		// 2. 입력받은 데이터를 변수에 저장 
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.print("이름: ");	
+		String 이름 = scanner.next();
+		System.out.print("나이: ");	
+		int 나이 = scanner.nextInt();
+		System.out.print("키: ");	
+		double 키 = scanner.nextDouble();
+		
+		// 3. 변수 출력 
+		System.out.println("--------- 개인정보 ---------");
+		System.out.printf("%5s \t %5s \t %5s\n", "이름","나이","키");
+		System.out.printf("%5s \t %5d \t %5.1f", 이름 , 나이 , 키 );
+		
 		
 	} // main e
 } // class e
 /*
+ 	System.out : 출력
+ 	System.in : 입력 
+ 	
  	형식문자 
  		1. %d : 정수
  		 	%자리수d	: 오른쪽부터 자리수만큼 자리 차지	[만약에 숫자가 없을경우 공백 채움 ]
