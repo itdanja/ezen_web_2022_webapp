c04bf9bc72538efb601993dd145832ba5e35cd70
504t <504t@DESKTOP-DE9LJ86>
2022-09-01 16:37:26 +0900

    day21

--- backend/src/java1/b연산자/EX1_연산자.java
+++ backend/src/java1/b연산자/EX1_연산자.java
@@ -12,7 +12,6 @@ public class EX1_연산자 { // class start
 		System.out.println(" 4.곱하기 : "+(정수1 * 정수2));
 		System.out.println(" 5.나누기 : "+(정수1 / 정수2) );
 		System.out.println(" 6.나머지 : "+(정수1 % 정수2) );
-		
 		// 2. 비교 
 		System.out.println(" 7.이상 : " + (정수1 >= 정수2) );
 		System.out.println(" 8.이하 : " + (정수1 <= 정수2) );
@@ -20,12 +19,10 @@ public class EX1_연산자 { // class start
 		System.out.println(" 10.미만 : " + (정수1 < 정수2) );
 		System.out.println(" 11.같다 : " + (정수1 == 정수2) );
 		System.out.println(" 12.같지않다 : " + (정수1 != 정수2) );
-		
 		// 3. 논리
 		int 정수3 = 30; int 정수4 = 40;
 		System.out.println(" 13.&&[AND] " + (정수1 >= 정수2 && 정수3 >= 정수4));
 		System.out.println(" 14.||[OR] " + (정수1 >= 정수2 || 정수4 >= 정수3 ));
-		
 		// 4. 증감 
 		int 정수5 = 10;
 		System.out.println(" 후위증가 : " + (정수5++) );	// 출력 후에 1 증가 
@@ -35,11 +32,27 @@ public class EX1_연산자 { // class start
 		System.out.println(" 확인 : " + 정수5 );
 		System.out.println(" 선위감소 : " + (--정수5) );  	// 출력 전에 1 감소
 		
+		// 5. 대입연산자 
+		int 정수6 = 10;	// = : 대입
+		정수6 += 3 ;		// += : 오른쪽 데이터를 왼쪽에 더한 후에 대입 
+			// 1.  정수6 + 3  -> 13       2. 정수6 = 13
+		System.out.println(" 대입 결과 : " + 정수6);
+		
+		정수6 -= 5;		// 1. 정수6-5 -> 8   2. 정수6 = 8
+		System.out.println(" 대입 결과 : " + 정수6 );
+		
+		// 6. 삼항연산자 
+		int 점수 = 85;
+		char 등급 = 점수 > 90 ? 'A' : 'B';	// 조건식 ? 참 : 거짓 
+				 // 만약에 점수가 90점 초과이면 'A' 대입 아니면 'B'대입
+		System.out.println(" 등급 결과 : " + 등급 );
+		
+		char 등급2 = 점수 > 90 ? 'A' : 점수 > 80 ? 'B' : 'C' ;
+		System.out.println(" 등급 결과 : " + 등급2 );
 		
 		
-	}
-	
-
+		
+	} // main end 
 }// class end 
 
 /*
