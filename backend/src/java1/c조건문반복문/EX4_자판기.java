c972e2d4d48bbfcb6c0e9da4c0b9a562807f8da4
504t <504t@DESKTOP-DE9LJ86>
2022-09-02 16:27:36 +0900

    day22

--- backend/src/java1/c조건문반복문/EX4_자판기.java
+++ backend/src/java1/c조건문반복문/EX4_자판기.java
@@ -11,7 +11,7 @@ public class EX4_자판기 { // class start
 		// 1. 공통 변수( 제품3개->3개 , 장바구니제품3개->3개 )
 		int 콜라재고 = 10;		int 환타재고 = 10;		int 사이다재고 = 10;	// 1. 각 변수에 10 대입 [ 재고 ]
 		int 콜라구매수 = 0;	int 환타구매수 = 0;	int 사이다구매수 = 0;
-
+		
 		while(true) { // 2. 무한루프 [ 무한반복 ]
 			System.out.println("---------[ 자판기 ]-----------");
 			System.out.println("메뉴 : 1.콜라(300) 2.환타(400) 3.사이다(500) 4.결제 ");
@@ -24,7 +24,7 @@ public class EX4_자판기 { // class start
 				}else { // 재고 없다 
 					System.err.println("안내) 콜라재고가 없습니다. [ 죄송합니다ㅠ]");
 				}
-			} 
+			} // elseif 1 end 
 			else if( 메뉴선택 == 2 ) {  //  2 를 입력했을때 
 				// 재고 있다/없다 [ 경우의수 2개 ]
 				if( 환타재고 > 0 ) { // 재고 있다
@@ -33,7 +33,7 @@ public class EX4_자판기 { // class start
 				}else { // 재고 없다 
 					System.err.println("안내) 환타재고가 없습니다. [ 죄송합니다ㅠ]");
 				}
-			}
+			} // elseif 2 end 
 			else if( 메뉴선택 == 3 ) {  //  3 를 입력했을때 
 				// 재고 있다/없다 [ 경우의수 2개 ]
 				if( 사이다재고 > 0 ) { // 재고 있다
@@ -42,10 +42,31 @@ public class EX4_자판기 { // class start
 				}else { // 재고 없다 
 					System.err.println("안내) 사이다재고가 없습니다. [ 죄송합니다ㅠ]");
 				}
-			}
+			} // elseif 3  end 
 			else if( 메뉴선택 == 4 ) {  //  4 를 입력했을때 
+				System.out.println("----------- 장바구니 -------------"); // 선택된 제품의 목록 
+				System.out.println("제품명\t구매수\t가격");
+				if( 콜라구매수 > 0 ) { System.out.println("콜라\t"+콜라구매수+"\t"+(콜라구매수*300)+ "원"); }
+				if( 환타구매수 > 0 ) { System.out.println("환타\t"+환타구매수+"\t"+(환타구매수*400)+ "원"); }
+				if( 사이다구매수 > 0 ) { System.out.println("사이다\t"+사이다구매수+"\t"+(사이다구매수*500)+ "원"); }
 				
-			}
+				int 총결제금액 = (콜라구매수*300)+(환타구매수*400)+(사이다구매수*500) ;
+				System.out.println("안내) 총 결제예정금액 : " + 총결제금액 + "원" );
+				
+				System.out.print("안내) 결제 하시겠습니까? [Y] [N] "); String 결제여부 = scanner.next();
+				
+				if( 결제여부.equals("Y") || 결제여부.equals("y") ) {
+					
+					System.out.print("안내) 결제금액을 투여 해주세요 : "); int 투입금액 = scanner.nextInt();
+					if( 총결제금액 > 투입금액 ) { System.out.println("안내) 금액이 부족합니다. [결제취소]" ); }
+					else { System.out.println("안내) 구매 완료 번호:0000 [ 잔돈 : "+(투입금액-총결제금액) +" 원]"); }
+					
+				} // if end 
+				
+				// 결제취소 : 금액이 부족 하거나 . 결제여부가 N 이면 	=> 재고를 구매수만큼 다시 채우기 
+				// 결제성공 : 장바구니 0 으로 초기화 
+				
+			} // elseif 4  end 
 			else { System.err.println("안내) 선택할수 없는 번호입니다[다시선택] "); } // 그외 입력했을때 
 		} // while end [ 무한루프 종료 ]		
 	} // main end 
