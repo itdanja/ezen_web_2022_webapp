b451f764b8489f5a30bebfcb232ef56e5d9d3d9f
504t <504t@DESKTOP-DE9LJ86>
2022-09-05 12:03:28 +0900

    day23_별찍기

---
+++ backend/src/java1/c조건문반복문/EX7_별찍기.java
@@ -0,0 +1,52 @@
+package java1.c조건문반복문;
+
+import java.util.Scanner; // 외부 클래스 호출[ 가져오기 ] // 자동완성 아닐경우 직접 입력!!!
+
+public class EX7_별찍기 {
+
+	public static void main(String[] args) {
+		
+		// 0. 입력객체 [ 1번 선언 ]
+		Scanner scanner = new Scanner(System.in);
+		
+		// 1. 입력받은 수 만큼 * 출력 
+		System.out.print("문제1) 별개수 : "); int s1 = scanner.nextInt();
+		for( int i = 1 ; i <= s1 ; i++ ) { // i는 1부터 입력받은 수까지 1씩 증가반복
+			System.out.print("*");
+		} // for end 
+System.out.println("\n---------------------------------------------------");
+
+		// 2. 입력받은 수 만큼 * 출력 [ 3줄마다(3배수) 줄바꿈 처리 ]
+		System.out.print("문제2) 별개수 : "); int s2 = scanner.nextInt();
+		for( int i = 1 ; i<= s2 ; i++ ) {
+			System.out.print("*"); if( i % 3 == 0 ) { System.out.println(); }
+		} // for end 
+System.out.println("\n---------------------------------------------------");
+
+		// 3.
+		System.out.print("문제3) 줄수 : "); int line3 = scanner.nextInt();
+		for( int i = 1 ; i<=line3 ; i++ ) {
+			// 별
+			for( int s = 1 ; s<= i ; s++ ) { System.out.print("*"); }
+			// 줄바꿈
+			System.out.println();
+		} // for end 
+System.out.println("\n---------------------------------------------------");
+	
+		// 4.
+
+
+
+
+
+
+
+
+
+
+
+
+
+		
+	} // main end 
+} // class end 
