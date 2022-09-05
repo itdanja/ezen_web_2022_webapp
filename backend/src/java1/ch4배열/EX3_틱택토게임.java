e6146985e05e512363da0502fc48112c5ac83bbc
504t <504t@DESKTOP-DE9LJ86>
2022-09-05 17:46:45 +0900

    day23_틱택토 컴퓨터난수생성

--- backend/src/java1/ch4배열/EX3_틱택토게임.java
+++ backend/src/java1/ch4배열/EX3_틱택토게임.java
@@ -1,5 +1,6 @@
 package java1.ch4배열;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class EX3_틱택토게임 {
@@ -12,25 +13,31 @@ public class EX3_틱택토게임 {
 			
 			// 1. 게임판 출력 - 배열내 모든 데이터 출력 
 			for( int i = 0 ; i<게임판.length ; i++ ) { // i는 0부터 마지막인덱스( 배열의길이-1 ) 까지 1씩 증가
-				
 				System.out.print( 게임판[i] );
 				// 1-2 3줄[ 인덱스 : 2 , 5 , 8 ] 마다 줄바꿈
 				if( i == 2 || i == 5 || i == 8 ) { System.out.println(); }
 				// if( i == 2 || i == 5 || i == 8 ) ---> if( i % 3 == 2 )
-				
 			} // for end 
+			
 			// 2. 사용자 에게 위치 입력받기 
 			while( true ) { // 무한루프 [ 종료조건 : 알을 두었을때 종료 break ] // while 2
 				System.out.print("안내) 위치 선택 : "); int 위치 = scanner.nextInt();
 				if( 위치 < 0 || 위치 > 8 ) { System.out.println("안내) 선택할수 없는 번호입니다.");}
 				// 해당 위치에 알이 없으면 해당 위치에 알두기 
-				if( 게임판[위치] .equals("[ ]") ) {  게임판[위치] = "[O]"; break; }
+				if( 게임판[위치].equals("[ ]") ) {  게임판[위치] = "[O]"; break; } // 알을 두면 무한루프 종료
 				else { System.out.println("안내) 이미 알이 존재하는 위치 입니다. "); }
 			} // while 2 end 
+			
 			// 3. 컴퓨터 에게 위치 난수 생성
+			while( true ) { // while 2
+				Random random = new Random(); // 랜덤 객체 생성 
+				int 위치 = random.nextInt(9); // 0~8 난수 생성 
+				if( 게임판[위치].equals("[ ]")){ 게임판[위치] = "[X]"; break; } // 알을 두면 무한루프 종료
+			} // while 2 end 
+			
+			// 4. 승리 판단 [ 과제 ] 
 			
 		} // while 1  end 
-		
 	} // main end 
 } // class end 
 /*
