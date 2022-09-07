5e7c7fd1a02f315ad7b285af4f537502a3dc8571
504t <504t@DESKTOP-DE9LJ86>
2022-09-07 10:03:37 +0900

    과제체크

---
+++ backend/src/과제2_비회원제게시판/최유정.java
@@ -0,0 +1,101 @@
+package 과제2_비회원제게시판;
+
+import java.util.Scanner;
+
+public class 최유정 {//class s
+
+	public static void main(String[] args) {//main s
+	      
+	      Scanner scanner = new Scanner(System.in);
+	      String[][] 게시물 = new String[100][4];
+	      
+	       while(true) { 
+	               System.out.println("--------- 커뮤니티 ----------");
+	               System.out.printf("번호\t작성자\t제목\n");
+	               for( int i=0; i<게시물.length; i++ ) {   
+	                  if( 게시물[i][0] != null ) {      // 글이 존재할경우에만 출력 
+	                     System.out.print(i+"\t");
+	                     System.out.print(게시물[i][2]+"\t");
+	                     System.out.print(게시물[i][0]+"\n");
+	                  }
+	               }//for e               
+	               
+	               System.out.println("1.글쓰기\t2.글보기\t선택");      int 선택 = scanner.nextInt();
+
+	               if(선택==1) {
+	                  System.out.println("안내) 글쓰기 페이지입니다.");
+	                  System.out.print("title : ");            String title = scanner.next();
+	                  System.out.print("content : ");            String content = scanner.next();
+	                  System.out.print("writer : ");            String writer = scanner.next();
+	                  System.out.print("password : ");         String password = scanner.next();
+	                  
+	                  for( int i=0; i<게시물.length; i++ ) {   // 배열내 모든 항목 출력 
+	                        if( 게시물[i][0] == null ) {   // 공백 이면 
+	                           게시물[i][0] = title;         // 정보 저장하고 
+	                           게시물[i][1] = content;
+	                           게시물[i][2] = writer;
+	                           게시물[i][3] = password;
+	                           break;                  // 나간다.
+	                        }
+	                  }//for e      
+	               }//if e
+	               
+	               
+	               else if( 선택 == 2 ) {
+	                  System.out.println(" 글 번호  선택 : "); int index = scanner.nextInt();
+	                  System.out.println("----------- 게시물 상세페이지 -------------------");
+	                  System.out.println( "작성자 : "+게시물[index][2] + "   제목 : "+ 게시물[index][0]   );
+	                  System.out.println( "내용 : "+게시물[index][1] );
+	                  System.out.println("1.뒤로가기 2.글삭제 3.글수정 : ");
+	                  int 선택2 = scanner.nextInt();
+	                  if( 선택2 == 1 ) {}
+	                  else if( 선택2 == 2 ) {
+	                     System.out.println("글삭제 페이지입니다.");
+	                     System.out.println("삭제할 글 번호  선택 : "); int index1 = scanner.nextInt();
+	                     System.out.println("글 작성시 사용한 비밀번호를 입력해주세요"); String 비밀번호 = scanner.next();
+	                    
+	                        if( 게시물[index1][3].equals(비밀번호) ) {
+	                           게시물[index1][0] = null;
+	                           게시물[index1][1] = null;
+	                           게시물[index1][2] = null;
+	                           게시물[index1][3] = null;
+	                           System.out.println("해당 게시물이 삭제되었습니다.");
+	                        }
+	                        else {
+	                           System.out.println("비밀번호가 일치하지 않습니다.");
+	                        }
+	                        
+	                        //if e
+	                           
+	                           
+	                 }
+	                  else if( 선택2 == 3 ) {
+	                     System.out.println("글 수정 페이지 입니다.");
+	                     System.out.println("수정할 글 번호  선택 : "); int index1 = scanner.nextInt();
+	                     System.out.println("글 작성시 사용한 비밀번호를 입력해주세요"); String 비밀번호 = scanner.next();
+	                     if( 게시물[index1][3].equals(비밀번호) ) {
+	                        System.out.println("비밀번호가 일치합니다. 수정할 내용을 입력해주세요");
+	                        System.out.println("수정할 title : "); String title1 = scanner.next();
+	                        System.out.println("수정할 content :"); String content1 = scanner.next();
+	                        System.out.println("수정할 writer :"); String writer1 = scanner.next();
+	                        System.out.println("수정할 password :"); String password1 = scanner.next();
+	                        
+	                        게시물[index1][0] = title1;
+	                        게시물[index1][1] = content1;
+	                        게시물[index1][2] = writer1;
+	                        게시물[index1][3] = password1;
+	                        
+	                        System.out.println(index1+"번째 게시물 수정 완료");
+	                     
+	                     
+	                     }
+	                  
+	                  }//else if e
+	                  
+	               }//else if e
+	               
+	               
+	               
+	       }//while e
+	      }//main e
+	   }//class e
