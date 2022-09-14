1fd6b8b37bdfbdf5cebc93bd00d179c64b089c03
504t <504t@DESKTOP-DE9LJ86>
2022-09-14 12:09:11 +0900

    day27

--- backend/src/DATABAES/java연동/JDBC실행.java
+++ backend/src/DATABAES/java연동/JDBC실행.java
@@ -1,8 +1,9 @@
 package DATABAES.java연동;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
+import java.sql.DriverManager;		// db 드라이버 클래스 [ 드라이버 데이터 ]
+import java.sql.Connection;			// db연동 인터페이스 
+import java.sql.PreparedStatement;	// sql 조작 인터페이스 
+import java.sql.ResultSet;			// sql 결과 조작 인터페이스  
 
 public class JDBC실행 {
 
@@ -40,6 +41,7 @@ public class JDBC실행 {
 			
 		// * JDBC에서 SQL 조작 
 		// 1. 테이블에 데이터 추가 
+		/*
 			// 1. sql 문법을 " " 감싸고 문자열 변수 에 저장 
 			String sql = "insert into member values('tess','나훈아','경기 부천시 중동')";
 			// 2. sql 조작 인터페이스
@@ -59,6 +61,36 @@ public class JDBC실행 {
 			sql = "insert into member values('jyp' , '박진영' , '경기 고양시 장항동')";
 			ps = con.prepareStatement(sql);
 			ps.executeUpdate();
+		*/
+		/*
+			// 박진영의 주소 변경 
+			String sql = "update member set member_addr = '경기도 안산시 상록구' where member_id ='jyp'";
+			PreparedStatement ps = con.prepareStatement(sql);
+			ps.executeUpdate();
+			
+			// 아이유 레코드 삭제 
+			sql = "delete from member where member_id='iyou'";
+			ps = con.prepareStatement(sql);
+			ps.executeUpdate();
+		*/
+			// 모든 멤버 호출 
+			String sql = "select * from member";
+			PreparedStatement ps = con.prepareStatement(sql);
+			ResultSet rs =  ps.executeQuery(); // 쿼리[ SQL 결과 ] 
+				// 결과가 레코드[행] 여러개 존재하기 때문에 ResultSet 인터페이스 저장 
+				// 반복문 이용한 레코드 하나씩 출력 
+					// null -> 검색레코드1 -> 검색레코드2  
+					// .next() : 다음 레코드 호출  [ 다음 레코드가 있으면 true 없으면 false 반환 ]
+					// .getString( 필드순서번호 ) 	: 해당 필드의 데이터를 문자열로 가져오기 
+					// .getInt( 필드순서번호 )		: 해당 필드의 데이터를 정수형을 가져오기 
+			while( rs.next() ) { // while (조건) : 조건이 true 이면 반복문 실행 
+				// rs.next() : 다음 레코드 이동 
+				System.out.print("회원아이디 : " +rs.getString(1) +"\t"); // 첫번째 필드의 데이터 가져오기 [ member_id ]
+				System.out.print("회원명 : " +rs.getString(2) +"\t" );	// 두번째 필드의 데이터 가져오기 [ member_name ]
+				System.out.print("회원주소 : " +rs.getString(3) +"\n" );	// 세번재 필드의 데이터 가져오기 [ member_addr ]
+			} 
+				
+			
 			
 			
 		}catch( Exception e ) { System.out.println("연동 실패 : " + e ); }
--- backend/src/DATABAES/java연동/JDBC실행2.java
+++ backend/src/DATABAES/java연동/JDBC실행2.java
@@ -1,5 +1,44 @@
 package DATABAES.java연동;
 
-public class JDBC실행2 {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 
+public class JDBC실행2 {
+	
+	public static void main(String[] args) {
+		try {
+		// 1. DB연동 
+		Connection con = DriverManager.getConnection( 
+				"jdbc:mysql://localhost:3306/shop_db3", 
+				"root",
+				"1234");
+		System.out.println("연동성공");
+		
+		// 2. SQL 작성
+		String sql = "insert into product values('바나나',1500,'2021-07-01','델몬트',17)";
+		// 3. SQL 조작[연결] 
+		PreparedStatement ps = con.prepareStatement(sql);
+		// 4. SQL 실행 
+		ps.executeUpdate();
+		
+		sql = "insert into product values('카스',2500,'2022-03-01','OB', 3 )";
+		ps = con.prepareStatement(sql);
+		ps.executeUpdate();
+		
+		sql = "insert into product values('삼각김밥',800,'2023-09-01','CJ',22)";
+		ps = con.prepareStatement(sql);
+		ps.executeUpdate();
+		
+		// 1. 카스의 재고수량을 10로 수정
+		
+		// 2. 삼각김밥 제거 
+		
+		// 3. 모든 제품의 정보를 console 출력 
+		
+		
+		
+		}catch (Exception e) { System.out.println("오류발생 : " + e );}
+
+	}
 }
