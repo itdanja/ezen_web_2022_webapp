3443ee184248275bf3ebc260faecdb546a55126e
504t <504t@DESKTOP-DE9LJ86>
2022-09-16 14:12:55 +0900

    day29 등록dao

--- backend/src/java1/ch5클래스/EX13_비회원게시판_DB버전/BoardDao.java
+++ backend/src/java1/ch5클래스/EX13_비회원게시판_DB버전/BoardDao.java
@@ -29,7 +29,26 @@ public class BoardDao {
 	// 3. 메소드 
 		// 1. 등록 메소드
 	boolean regist( BoardDto boardDto ) {
-		return false;
+			// 1. sql 작성
+		String sql = "insert "
+						+ "	into "
+						+ "		board( b_title , b_content , b_writer , b_password , b_view ) "
+						+ "	values( ? , ? , ? , ? , ? )";
+					// ? : 변수 들어갈 자리 [ 와일드카드 ] 
+			// 2. sql 연결 조작 
+		try {
+			ps = con.prepareStatement(sql);	// 위에서 작성된 sql를 ps인터페이스에 연결 
+				// * : ? 에 변수 넣기
+				ps.setString( 1 , boardDto.b_title );	 	// 1 : 첫번재 ? 	, 제목  
+				ps.setString( 2 , boardDto.b_content );	 	// 2 : 두번재 ? 	, 내용  
+				ps.setString( 3 , boardDto.b_writer );		// 3 : 세번째 ?	, 작성자 
+				ps.setString( 4 , boardDto.b_password );	// 4 : 네번째 ?  , 패스워드
+				ps.setInt( 5 ,  0 );						// 5 : 다섯번째 ? , 0     게시물등록시 조회수 0 시작
+			ps.executeUpdate();				// ps 인터페이스 sql 실행 
+			// 3. sql 결과 
+			return true;	// 등록 성공시 true 
+		}catch (Exception e) { System.out.println("경고) DB오류 : " + e); }
+		return false; // 등록 실패시 false 
 	}
 		// 2. 모든 게시물 출력 메소드 
 	BoardDto[] getBoardlist( ) {
