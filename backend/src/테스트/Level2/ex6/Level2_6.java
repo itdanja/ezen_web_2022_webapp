64f1c877ed1f00f5e4375c0916b48aa72ba44bd6
504t <504t@DESKTOP-DE9LJ86>
2022-09-26 17:44:25 +0900

    .

---
+++ backend/src/테스트/Level2/ex6/Level2_6.java
@@ -0,0 +1,26 @@
+package 테스트.Level2.ex6;
+
+import java.time.LocalDateTime;
+import java.util.Scanner;
+
+public class Level2_6 {
+	
+	public static void main(String[] args) {
+		System.out.println(" >> Level2_6 answer >> ");
+		System.out.println(" >> answer time : " + LocalDateTime.now() +" >> \n" );
+		Scanner scanner = new Scanner(System.in);
+		
+		
+		/* 문제풀이 위치 */
+	
+		/* ----------- */
+
+	}
+}
+/* 
+	[문제] 그림과 같이 무한 입력 받다가 end 입력시 종료되는 반복문을 구현하시오.
+	[조건] 
+		1. 문제풀이 위치 외 코드는 수정 불가 합니다.
+	
+		
+*/
---
+++ backend/src/테스트/Level2/ex7/Level2_7.java
@@ -0,0 +1,26 @@
+package 테스트.Level2.ex7;
+
+import java.time.LocalDateTime;
+import java.util.Scanner;
+
+public class Level2_7 {
+	
+	public static void main(String[] args) {
+		System.out.println(" >> Level2_7 answer >> ");
+		System.out.println(" >> answer time : " + LocalDateTime.now() +" >> \n" );
+		Scanner scanner = new Scanner(System.in);
+	
+		while(true) {
+			System.out.print("배수 입력 :"); int input = scanner.nextInt();
+		/* 문제풀이 위치 */
+	
+		/* ----------- */
+		}
+	}
+}
+/* 
+	[문제] 다음과 같이 배수를 무한입력을 받아 1~100 사이의 입력받은 배수 만 출력하시오.
+	[조건] 
+		1. 문제풀이 위치 외 코드는 수정 불가 합니다.
+
+*/
