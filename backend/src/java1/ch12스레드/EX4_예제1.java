8e1fcdf89b847718970339f9f9899ef8399aaeb9
504t <504t@DESKTOP-DE9LJ86>
2022-09-26 15:20:44 +0900

    day35 db

--- backend/src/java1/ch12스레드/EX4_예제1.java
+++ backend/src/java1/ch12스레드/EX4_예제1.java
@@ -4,12 +4,14 @@ import java.util.Scanner;
 
 public class EX4_예제1 {
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		boolean musicstate = false;	// 음악 재생여부 판단 변수 
-		Music music = new Music();  // 음악 스레드 선언 
 		
-		boolean moviestate = false; // 영화 재생여부 판단 변수 
-		Movie movie = new Movie();	
+		Scanner scanner = new Scanner(System.in);
+		
+		boolean musicstate = false;			// 음악 재생여부 판단 변수 
+		Music music = new Music();  		// 음악 스레드 선언 
+		
+		boolean moviestate = false; 		// 영화 재생여부 판단 변수 
+		Movie movie = new Movie();		
 		Thread thread = new Thread(movie); // 영화 스레드 선언 
 		
 		while( true ) {
@@ -20,18 +22,21 @@ public class EX4_예제1 {
 			
 			int ch = scanner.nextInt();
 			
-			if( ch == 1 && musicstate == false ) {  // 음악스레드 시작
-				music.start();			
-				musicstate = true;
-			} else if( ch == 1 && musicstate == true ) {
-				music.setStop(false);
-				musicstate = false;		music = new Music();
+			if( ch == 1 && musicstate == false ) { // 입력한 값이 1번 이면서 음악스레드가 꺼져있으면
+				music.start();				// 음악 스레드 실행 		
+				musicstate = true;			// 현재 스레드가 작업 스레드 재생여부 변경
+			} else if( ch == 1 && musicstate == true ) { // 입력한 값이 1번 이면서 음악스레드 가 켜져있으면
+				musicstate = false; 		// 현재 스레드가 작업 스레드 재생여부 변경
+				music.setStop(false);		// 음악 스레드 종료 -> while 종료 -> run메소드 종료 -> 멀티스레드 종료
+				music = new Music();		// 새로운 메모리 할당
 			}
 			if( ch == 2 && moviestate == false ) {
 				thread.start();	moviestate = true;
 			} else if( ch == 2 && moviestate == true ) {
 				movie.setStop(false);
-				moviestate = false;	thread = new Thread(movie);
+				moviestate = false;	
+				movie = new Movie();	
+				thread = new Thread(movie);
 			}
 			
 		}
