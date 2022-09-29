844e96bb26fe6c9359a7863810e62d405638c1ab
504t <504t@DESKTOP-DE9LJ86>
2022-09-29 11:13:02 +0900

    day37

---
+++ backend/src/java1/ch07상속/EX5_타이어예제/Car.java
@@ -0,0 +1,25 @@
+package java1.ch07상속.EX5_타이어예제;
+
+public class Car {
+	// 1. 필드 [ 변수 , 객체 , 상수 , 배열, 리스트 등 = 메모리 ]
+	Tire frontLeftTire = new Tire( 6 , "앞왼쪽");
+	Tire frontRightTire = new Tire(2 , "앞오른쪽");
+	Tire backLeftTire =  new Tire(3 , "뒤왼쪽");
+	Tire backRightTire =  new Tire(4 , "뒤오른쪽");
+	
+	// 2. 생성자
+	
+	// 3. 메소드 
+	int run() {
+		System.out.println("[자동차가 달립니다.]");
+		if( frontLeftTire.roll() == false ) { stop(); 	return 1; }
+		if( frontRightTire.roll() == false ){ stop(); 	return 2; }
+		if( backLeftTire.roll() == false ) 	{ stop(); 	return 3; }
+		if( backRightTire.roll() == false ) { stop(); 	return 4; }
+		return 0;
+	}
+
+	
+	void stop() {System.out.println("[자동차가 멈춥니다.]");}
+	
+}
