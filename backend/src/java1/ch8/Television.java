9b270836aabf1ef5c6b338d7c8851c0bdd10fceb
504t <504t@DESKTOP-DE9LJ86>
2022-10-04 10:21:36 +0900

    day39_인터페이스

---
+++ backend/src/java1/ch8/Television.java
@@ -0,0 +1,27 @@
+package java1.ch8;
+
+public class Television implements RemoteControl {
+			// [ implements 인터페이스 ] : 구현하다 ( 추상을 구현 )
+			// [ extends 클래스 ] : 연장하다 ( 상속 ) 
+	// 1. 필드
+	private int volume;
+	// 2. 생성자 
+	// 3. 메소드 
+	// RemoteControl 인터페이스의 추상메소드를 구현하자
+	@Override
+	public void turnOn() { System.out.println(" TV를 켭니다."); }
+	@Override
+	public void turnOff(){ System.out.println(" TV를 끕니다."); }
+	@Override
+	public void setVolume(int volume) {
+		// 만일 현재 tv 소음이 리모콘의 최대소음보다 크면 
+		if( volume > RemoteControl.MAX_VOLUME ) {
+			this.volume = RemoteControl.MAX_VOLUME; // tv소음을 10 고정 
+		}else if ( volume < RemoteControl.MIN_VOLUME ) {
+			this.volume = RemoteControl.MIN_VOLUME;	
+		}else {
+			this.volume = volume;
+		}
+	}
+	
+}
