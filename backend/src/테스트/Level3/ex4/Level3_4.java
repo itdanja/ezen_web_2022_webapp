d8ff46b3ef3e2a01acde74dc988d59c009b9f392
MSI <kgs2072@naver.com>
2022-10-04 23:50:52 +0900

    테스트

---
+++ backend/src/테스트/Level3/ex4/Level3_4.java
@@ -0,0 +1,29 @@
+package 테스트.Level3.ex4;
+
+import java.time.LocalDateTime;
+import java.util.Scanner;
+
+public class Level3_4 {
+	
+	public static void main(String[] args) {
+		System.out.println(" >> Level3_4 answer >> ");
+		System.out.println(" >> answer time : " + LocalDateTime.now() +" >> \n" );
+		Scanner scanner = new Scanner(System.in);
+	
+		/* 문제풀이 위치 */
+
+		/* ----------- */
+
+	}
+}
+/* 
+	[문제] Level3_2 이어서 작성합니다. 중복체크를 진행합니다.
+	[조건] 
+		1. 문제풀이 위치 외 코드는 수정 불가 합니다.
+		2. 만일 배열에 null 존재할경우 출력하지 않습니다.
+		3. [ 중복체크 ] 입력한 데이터가 이미 배열에 존재할 경우 다시 입력받습니다.
+		4. [ 친구삭제 ] 삭제할 친구 이름을 입력받아 삭제를 진행합니다. [ 삭제시 삭제된 인덱스뒤로 한칸씩 당겨줍니다 ]
+		5. [ 친구추가 ] 추가할 친구 이름을 입력받아 배열에 비어 있는 자리가 있으면 추가합니다..
+		 			  만일 빈자리가 없을경우 해당 배열의 길이를 +1 증가시켜 배열을 저장합니다.
+		6. [ 재등록 ] 처음 코드로 돌아가서 친구 수를 다시 입력받습니다.
+*/
