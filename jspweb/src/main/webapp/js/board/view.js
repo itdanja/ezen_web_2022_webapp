a3b7a5311a6c611fd3429c82f7ceba73818e8d05
504t <504t@DESKTOP-DE9LJ86>
2022-10-14 15:58:12 +0900

    day46 게시글 삭제

--- jspweb/src/main/webapp/js/board/view.js
+++ jspweb/src/main/webapp/js/board/view.js
@@ -22,20 +22,58 @@ function bview(){
 			console.log( board.bfile )
 			
 			if( board.bfile !== null ){	// null , undefined , 0 , false
-				alert('null 이 아닌경우 ')
 				let filelink = '<a href="../board/filedown?bfile='+board.bfile+'">'+board.bfile+'</a>'
 				// ' ' : 전체 문자열 처리
 				// " " : 전체 문자열내 문자열 구분  
 				document.querySelector('.bfile').innerHTML = filelink;
-			}else{
-				alert('null 입니다.')
 			}
-			 
 			
+			console.log( board.btnaction )
+			let btnbox = document.querySelector('.btnbox')
 			
+			if( board.btnaction == true ){
+				// 삭제 버튼 활성화
+				let deletebtn = '<button onclick="bdelete('+board.bno+')"> 삭제 </button>'
+				btnbox.innerHTML += deletebtn;
+				// 수정 버튼 활성화 
+				let updatebtn = '<button><a href="../board/update.jsp">수정</a></button>'
+				btnbox.innerHTML += updatebtn;
+			}
 		}
 	})
 }
+
+
+function bdelete( bno ){ // 삭제 버튼 클릭시 삭제할 번호를 인수[식별]
+	$.ajax({
+		url : "http://localhost:8080/jspweb/board/bdelete" , 
+		data : { "bno" : bno } , // 삭제할 게시물의 식별번호[pk->bno]
+		success : function( re ){
+			if( re === 'true'){
+				alert('글삭제 성공 ');
+				location.href="../board/list.jsp"
+			}
+			else{ alert('글삭제 실패 : [관리자에게문의]') }
+		}
+	})
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
