f94ac8ec789018753b6eef2277ed605b90748e71
504t <504t@DESKTOP-DE9LJ86>
2022-10-28 12:29:47 +0900

    day56 메인페이지 제품 출력

--- jspweb/src/main/webapp/js/index.js
+++ jspweb/src/main/webapp/js/index.js
@@ -8,34 +8,31 @@ function getpactive1(){
 			let json = JSON.parse( re ) 
 			let html = '';
 			json.forEach( p => { 
-				 html += // ** 직접 하기 
+				 html += 	'<div class="item">	'+
+							'	<a href="#">  '+
+							'		<img src="/jspweb/admin/pimg/'+ p.pimg +'">'+
+							'	</a>'+
+							'	<div class="item_info">'+
+							'		<div class="item_title">'+ p.pname +'</div>'+
+							'		'+
+							'		<div class="item_size"> [FREE] </div>'+
+							'		'+
+							'		<div class="item_price"> '+p.pprice+' </div> '+
+							'		<div> '+
+							'			<span class="item_sale"> '+(p.pprice-(p.pprice*p.pdiscount) ).toLocaleString()+'원 </span>'+
+							'			<span class="item_discount"> '+ Math.round(p.pdiscount*100)+'% </span>'+
+							'		</div>	'+
+							'		<div class="item_review">찜수 54 리뷰수 412</div> '+
+							'		'+
+							'	</div>'+
+							'	<div>	'+
+							'		<span class="badge rounded-pill text-bg-warning">주문폭주</span>'+
+							'		<span class="badge rounded-pill text-bg-danger">1+1</span>'+
+							'	</div>'+
+							'</div>';	
 			})
 			document.querySelector('.itemlist').innerHTML += html;
 		}
 	})
 }
 
-	'<div class="item">	'+
-	'	<a href="#">  '+
-	'		<img src="/jspweb/img/p1.gif">'+
-	'	</a>'+
-	'	<div class="item_info">'+
-	'		<div class="item_title"> 	'+
-	'			트렌디한 크롭 기장의 덤블안감 카라넥 스웨이드 무스탕'+
-	'		</div>'+
-	'		'+
-	'		<div class="item_size"> [FREE] </div>'+
-	'		'+
-	'		<div class="item_price"> 200,000 원 </div> '+
-	'		<div> '+
-	'			<span class="item_sale"> 20,000원 </span>'+
-	'			<span class="item_discount"> 90% </span>'+
-	'		</div>	'+
-	'		<div class="item_review">찜수 54 리뷰수 412</div> '+
-	'		'+
-	'	</div>'+
-	'	<div>	'+
-	'		<span class="badge rounded-pill text-bg-warning">주문폭주</span>'+
-	'		<span class="badge rounded-pill text-bg-danger">1+1</span>'+
-	'	</div>'+
-	'</div>';	
