9c6333e983f2e0e518f471366464a0043782274e
504t <504t@DESKTOP-DE9LJ86>
2022-10-31 11:25:47 +0900

    DAY57 제품출력

--- jspweb/src/main/webapp/admin/stock.jsp
+++ jspweb/src/main/webapp/admin/stock.jsp
@@ -8,20 +8,63 @@
 </head>
 <body>
 
-	<div class="container">
-		<h3> 재고 관리 </h3>
+	<div class="container">	<!-- b : container 박스권 -->
 		
-		<select class="cselect">
-			<option> 카테고리 선택 </option>
-		</select>
+		<div class="row my-3">	<!-- b : row 하위태그 가로배치 -->
+			<h3> 재고 관리 </h3>
+			<div class="col-md-4"> <!-- b : form-select : width 100%  vs   col-md-4 : width 12/4 % -->
+				<select class="cselect form-select">
+				
+				</select>
+			</div>
+			<div class="col-md-4"> <!-- b : col-md-숫자 12그리중 숫자만큼 사이즈 -->
+				<select class="pselect form-select">
+					<option> 제품명 선택 </option>
+				</select>
+			</div>
+		</div>
 		
-		<select class="pselect">
-			<option> 제품명 선택 </option>
-		</select>
+		<div class="row my-3">	<!-- b : my-3 위아래 바깥여백[마진] -->
+			<div class="col-md-6">
+				<h3>재고 등록</h3>
+				<table class="table"> <!-- b: table 테이블 형식 -->
+					<tr> <td style="width: 20%;"> 카테고리번호 </td> 	<td class="rows"></td> </tr>
+					<tr> <td style="width: 20%;"> 제품번호 </td> 		<td class="rows"></td> </tr>
+					<tr> <td style="width: 20%;"> 제품명 </td> 		<td class="rows"></td> </tr>
+					<tr> <td style="width: 20%;"> 사이즈 </td> 		<td class="rows"> <input type="text" class="psize form-control"> </td> </tr>
+					<tr> <td style="width: 20%;"> 색상 </td> 			<td class="rows"> <input type="text" class="pcolor form-control"> </td> </tr>
+					<tr> <td style="width: 20%;"> 재고수량 </td> 		<td class="rows"> <input type="text" class="pstock form-control"> </td> </tr>
+					<tr> <td colspan="2"> <button type="button" class="form-control">등록</button> </td> </tr>
+				</table>
+			</div>
+			<div class="col-md-6">
+				<h3>재고 출력</h3>
+			
+			</div>
+		</div>
 		
 	</div>
+	
 	
 	<script type="text/javascript" src="/jspweb/js/admin/stock.js"></script>
 
 </body>
 </html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- jspweb/src/main/webapp/js/admin/stock.js
+++ jspweb/src/main/webapp/js/admin/stock.js
@@ -1,5 +1,8 @@
 /*
 
+	== : 같다 [ 값만 비교 ]
+	=== : 같다 [ 값 과 자료형 비교 ]
+
 	- 함수
 	1. 함수명()
 	2. function(인수){ }
@@ -13,15 +16,33 @@
 	
 	- 활용
 	.addEventListener( '이벤트명' , 함수호출[1~3] )
+		: 이벤트 실행 결과정보를 반환
 	.forEach( 인수 => { 코드 } )
 */
 
+let productlist = null; /* 모든 제품를 가지고 있는 리스트 선언 [ 전역 = 모든 함수 사용목적 ] */
 
-let cselect = document.querySelector('.cselect') // 카테고리 선택 상자 
-let pselect = document.querySelector('.pselect') // 제품 선택 상자
+let cselect = document.querySelector('.cselect') 	// 카테고리 선택 상자 
+let pselect = document.querySelector('.pselect') 	// 제품 선택 상자
+let rows = document.querySelectorAll('.rows') 		// 재고 등록 td 행 목록
 
-// 카테고리 선택를 클릭했을떄 제품출력 메소드 호출
-cselect.addEventListener( 'click' , getproduct( ) )
+// 카테고리목록에서 선택를 클릭했을떄 제품출력 메소드 호출
+cselect.addEventListener( 'click' , e => {
+	let pcno = e.currentTarget.value // 선택된 카테고리 번호
+	getproduct( pcno ); // 선택된 카테고리 번호 인수로 전달 
+})
+
+// 제품 목록에서 선택을 했을때 재고 등록 html 구성
+pselect.addEventListener( 'click' , e => {
+	let pno = e.currentTarget.value // 선택된 카테고리 번호
+	productlist.forEach( p => { 
+		if( p.pno == pno ){ // 해당 제품의 번호와 선택된 제품의번호와 같으면
+			rows[0].innerHTML = p.pcno
+			rows[1].innerHTML = p.pno
+			rows[2].innerHTML = p.pname
+		}
+	})
+})
 
 /*1. 페이지 열리면 모든 카테고리 select 넣어주기 */
 getcategory()
@@ -30,10 +51,10 @@ function getcategory(){
 		url : "/jspweb/board/pcategory" , 
 		type : 'get',
 		success : function( re ){
-			let json = JSON.parse( re )
+			let categorylist = JSON.parse( re ) // 선언
 			// 배열객체.forEach( 반복변수명[아무거나] , 인덱스 , 배열객체명 => { 실행코드 } )
-			let html = ''
-			json.forEach( c => {
+			let html = '<option> 카테고리 선택 </option>'
+			categorylist.forEach( c => {
 				html += `<option value=${c.pcno}> ${c.pcname}</option>`
 			})
 			cselect.innerHTML = html
@@ -41,9 +62,24 @@ function getcategory(){
 	})
 }
 /*2. 선택된 카테고리별 제품리스트를 select 넣어주기 */
-function getproduct(){
-	alert('선택된 카테고리의 제품 출력')
+function getproduct( pcno ){
+	$.ajax({
+		url : "/jspweb/admin/regist" , 
+		data : { "type" : 1 , "option" : "all"  } ,
+		type : 'get',
+		success : function( re ){
+			productlist = JSON.parse( re ) // 대입 
+			let html = ''
+			productlist.forEach( p => {
+				if( p.pcno == pcno ){ // 해당제품의 카테고리번호 와 선택된 카테고리 번호 와 같으며
+					html += `<option value=${p.pno}> ${p.pname}</option>`
+				}
+			})
+			pselect.innerHTML = html
+		}
+	});
 }
+
 
 
 
