f02b65ab9cb397162e9c4c9fa867c9b5ce307257
504t <504t@DESKTOP-DE9LJ86>
2022-11-04 11:36:26 +0900

    day61 아임포트

--- jspweb/src/main/webapp/js/product/cart.js
+++ jspweb/src/main/webapp/js/product/cart.js
@@ -6,7 +6,7 @@ function getcart(){
 		type : "get" , 
 		success : re =>{ 
 			let cartlist = JSON.parse( re )
-			let html = '<tr><th width="5%"> </th><th width="10%"> 이미지 </th><th width="40%"> 상품정보 </th><th width="15%"> 수량 </th> <th width="15%">가격</th> <th width="10%"> 비고 </th> </tr>';
+			let html = '<tr><th width="5%"><input type="checkbox"></th><th width="10%"> 이미지 </th><th width="40%"> 상품정보 </th><th width="15%"> 수량 </th> <th width="15%">가격</th> <th width="10%"> 비고 </th> </tr>';
 			cartlist.forEach( c => {
 				html +=  '	<tr>'+
 								'<td> <input type="checkbox"> </td>'+
@@ -19,8 +19,52 @@ function getcart(){
 				
 			})
 			document.querySelector('.cart_t').innerHTML = html
-			
 		}
 	})
 }
+
+
+
+/* 아임포트 [ 결제 API ] */
+
+function requestPay() {
+	var IMP = window.IMP; 
+	IMP.init("imp14103839"); // [본인]관리자 식별코드 [ 관리자 계정마다 다름] 
+  IMP.request_pay({ 
+      pg: "kcp",
+      pay_method: "card",
+      merchant_uid: "ORD20180131-0000011",   //주문번호
+      name: "노르웨이 회전 의자",
+      amount: 64900,                         // 숫자타입
+      buyer_email: "gildong@gmail.com",
+      buyer_name: "홍길동",
+      buyer_tel: "010-4242-4242",
+      buyer_addr: "서울특별시 강남구 신사동",
+      buyer_postcode: "01181"
+  }, function (rsp) { // callback
+      if (rsp.success) {
+          // 결제 성공 시 로직,
+      } else {
+          alert('결제성공');
+      }
+  });
+}
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
--- jspweb/src/main/webapp/product/cart.jsp
+++ jspweb/src/main/webapp/product/cart.jsp
@@ -14,10 +14,19 @@
 		<table class="table cart_t">
 			
 		</table>
+		
+		<button type="button">선택삭제</button>
+		<button type="button">전체삭제</button>
+		<button type="button">쇼핑하기</button>
+		<button onclick="requestPay()" type="button">결제하기</button>	<!-- 아임포트 결제 버튼 -->
 	</div>
+	
+	<!-- 아임포트[ 결제 API ] -->
+	<script type="text/javascript" src="https://cdn.iamport.kr/js/iamport.payment-1.2.0.js"></script>
+	
 	<script src="/jspweb/js/product/cart.js" type="text/javascript"></script>
 	
-	
-
 </body>
 </html>
+
+
